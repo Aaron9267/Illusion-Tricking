f2e5a52c5b9f335acffc03e4ec231d3729560f0f
Aaron <aaron.vaneijck@gmail.com>
2023-11-23 14:10:57 +1300

    update classes and prices

--- pages/classes.jsx
+++ pages/classes.jsx
@@ -8,9 +8,9 @@ const classes = () => {
         <div>
             <Hero 
             heading='Classes' 
-            message='In term 4 we will be introducing a tumbling class that runs after the tricking session on Saturday.' 
-            message1='The timetable for term 4 is below.'
-            message2='You can go to contact to enroll. $100 per term or $10 for a casual session'
+            message='The timetable for 2024 term 1 is below.' 
+            message1='Go to contact to enroll for a class. $125 for the term or $15 for casual sessions'
+            message2='We can also arrange private sessions for tricking, tumbling and acro. $60 per private'
             frame='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3028.322179626751!2d175.27110941604028!3d-40.62277630767857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d40f354c424d59b%3A0xa56b19dc84d8400b!2s33%20Victoria%20Street%2C%20Levin%205510!5e0!3m2!1sen!2snz!4v1680124780575!5m2!1sen!2snz' 
             />
             <Class />
