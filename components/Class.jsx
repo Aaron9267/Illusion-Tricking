6ba7298eb32095187c25603ed48b27bdcb6cf7cf
Aaron <aaron.vaneijck@gmail.com>
2023-12-07 13:26:09 +1300

    f

--- components/Class.jsx
+++ components/Class.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import timetable from '../public/holidaytimetable.jpg'
+import timetable from '../public/htimetable.jpg'
 
 const Class = () => {
     return(
