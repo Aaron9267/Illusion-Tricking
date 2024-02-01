4eae3263bbaf8dc56033d04f9cc9b8b6ab59eb56
Aaron <aaron.vaneijck@gmail.com>
2024-02-01 13:06:04 +1300

    2024

--- components/Class.jsx
+++ components/Class.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import timetable from '../public/htimetable.jpg'
+import timetable from '../public/timetable.jpg'
 
 const Class = () => {
     return(
--- pages/classes.jsx
+++ pages/classes.jsx
@@ -8,10 +8,10 @@ const classes = () => {
         <div>
             <Hero 
             heading='Classes' 
-            message='We will be collaborating with GYM Junkie for our 2024 holiday programme.' 
-            message1='Sessions will be running in their gym starting from January 15th to February 2nd'
+            message='Term 1 tricking will be starting back at the gymnastics club on the 12th of Feb and running to 13th April..' 
+            message1='All the classes have remained the same and we are adding an extra at Gym Junkie on Fridays. The timetable is as follows'
             message2='Go to contact to enroll, timetable and prices are below'
-            frame='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3027.8837380128875!2d175.27522217602174!3d-40.63244587140578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d40f34daeaab7c9%3A0x101f0803b5c5ee8d!2sGYM%20JUNKIE!5e0!3m2!1sen!2snz!4v1701906567511!5m2!1sen!2snz' 
+            frame='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12113.28797635662!2d175.2732981!3d-40.6227804!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d40f3c0a249b621%3A0x26a007b65b89dbfd!2sIllusion%20Tricking!5e0!3m2!1sen!2snz!4v1706745810973!5m2!1sen!2snz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade' 
             />
             <Class />
             <Instagram />
